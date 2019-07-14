bce0f01296f3a29b2d58983cd2057b13c8db4059
NickRoh <roh517@naver.com>
2019-07-14 15:08:27 +0900

    Update Vote_KMLA.cpp

--- Vote_KMLA.cpp
+++ Vote_KMLA.cpp
@@ -1,13 +1,18 @@
+
+
+
 #include <fstream>
 #include <cstdio>
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <stdlib.h>
+#include <windows.h>
 
-
+#define _CRT_SECURE_NO_WARNINGS
 //멍청한 input 예외처리
 
-#pragma warning(suppress : 4996) 
+#pragma warning(disable : 4996) 
 
 using namespace std;
 
@@ -38,6 +43,18 @@ bool sf(candidate a, candidate b)
 	if (a.polls != b.polls)
 	{
 		return a.polls > b.polls;
+	}
+	else
+	{
+		return a.ord < b.ord;
+	}
+}
+
+bool sff(candidate a, candidate b)
+{
+	if (a.points != b.points)
+	{
+		return a.points > b.points;
 	}
 	else
 	{
@@ -77,7 +94,8 @@ void file_open()// candidate ord.. // not only file_open --> file open & vote_2
  //    }
 
 
-	stream2 = freopen("C:\\Users\\roh51\\Desktop\\Codying\\test.txt", "r", stdin);
+	/*stream2 = freopen("C:\\Users\\roh51\\Desktop\\Codying\\test.txt", "r", stdin);*/
+	stream2 = freopen("vote2.txt", "r", stdin);
 	if (!stream2)
 	{
 		printf("Error -- File Not Found");
@@ -99,6 +117,7 @@ void file_open()// candidate ord.. // not only file_open --> file open & vote_2
 		}
 		puts("");
 	}*/
+	fclose(stream2);
 }
 
 
@@ -107,8 +126,8 @@ void cadidate_input()
 	printf("후보자는 몇명인가요?");
 	scanf(" %d", &n);
 	for (int i = 0; i < n; i++)
-	{	
-		printf("%d 번째 후보자의 번호를 입력해주세요",i+1);
+	{
+		printf("%d 번째 후보자의 번호를 입력해주세요", i + 1);
 		scanf(" %d", &candidate_1[i].ord);
 	}
 }
@@ -138,7 +157,7 @@ void voting_2_part2(int num_a, int num_b)
 		}
 	}
 	sort(candidate_2, candidate_2 + 2, sf);
-	cout << "당선자는 " <<candidate_2[0].ord << "번 입니다!!" << endl;
+	cout << "당선자는 " << candidate_2[0].ord << "번 입니다!!" << endl;
 }
 
 void voting_2(int a_num, int b_num, int c_num)
@@ -164,15 +183,25 @@ void voting_2(int a_num, int b_num, int c_num)
 			}
 		}
 	}
-	sort(candidate_2, candidate_2 + 3, sf);
+	sort(candidate_2, candidate_2 + 3, sff);
 	//cout << candidate_2[0].ord << endl;
 	//cout << candidate_2[1].ord << endl;
-	voting_2_part2(candidate_2[0].ord, candidate_2[1].ord);
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d 번째 후보자는 ", i + 1);
+		printf(" %d번 입니다---> ", candidate_2[i].ord);
+		printf("득표수는 %d 입니다 ", candidate_2[i].points);
+		puts("");
+	}
+
+	cout << "당선자는 " << candidate_2[0].ord << "번 입니다!!"<< endl;
+	//voting_2_part2(candidate_2[0].ord, candidate_2[1].ord);
 }
 
 void vote_1()
 {
-	stream=freopen("C:\\Users\\roh51\\Desktop\\Codying\\points.txt", "r", stdin);
+	/*stream = freopen("C:\\Users\\roh51\\Desktop\\Codying\\a.txt", "r", stdin);*/
+	stream = freopen("vote1.txt", "r", stdin);
 	if (!stream)
 	{
 		printf("Error File Not Found");
@@ -191,10 +220,15 @@ void vote_1()
 	fclose(stream);
 
 	sort(candidate_1, candidate_1 + n, sf); //동석차 예외 처리
-	/*for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		cout << candidate_1[i].ord << endl;
-	}*/
+		printf("%d 번째 후보자는 ", i + 1);
+		printf(" %d번 입니다---> ", candidate_1[i].ord);
+		printf("득표수는 %d 입니다 ", candidate_1[i].polls);
+		//cout << "" <<candidate_1[i].ord << endl;
+		//cout << candidate_1[i].polls << endl;
+		puts("");
+	}
 
 	for (int i = 0; i < 3; i++)
 	{
@@ -217,13 +251,35 @@ void vote_1()
 
 int main()
 {
+	int limit;
+	printf("결과를 몇초동안 보고 싶나요? 정수로 입력하세요\n");
+	scanf("%d",&limit);
+	puts("");
+
 	memset(&candidate_1, 0, sizeof(struct candidate));
 	memset(&candidate_2, 0, sizeof(struct candidate));
 
 	cadidate_input();
 	voter_num_input();
-	vote_1();;
+	vote_1();
+	printf("-------------------------------");
+	puts("");
+	printf("2번째 투표 결과입니다\n");
 	file_open();
 	voting_2(candidate_2[0].ord, candidate_2[1].ord, candidate_2[2].ord);
+	int time = 0;
+	while (1)
+	{
+		Sleep(1000);
+		time++;
+		if (time >= limit)
+		{
+			break;
+		}
+	}
+
 	return 0;
+
+	
+	
 }
